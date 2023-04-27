660ab856ec4fbebb2c98749d327cb0ad6564b053
officialheartwell <okorieheartwell@gmail.com>
2023-04-27 23:48:55 +0100

    learn about the header & main function

--- programming_basics/dummy.c
+++ programming_basics/dummy.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
+
 int main()
 /*The main function is refers to as the integer functions; i.e its return and integer to the operating system*/
 {
-    return (2);
+    puts("I am no longer a dummy.");
+    return 3;
 }
