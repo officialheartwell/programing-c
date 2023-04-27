295a7e45b1daebf2f42d4459e7bf3f25f674350a
officialheartwell <okorieheartwell@gmail.com>
2023-04-27 23:54:04 +0100

    updated the code

--- programming_basics/dummy.c
+++ programming_basics/dummy.c
@@ -3,6 +3,6 @@
 int main()
 /*The main function is refers to as the integer functions; i.e its return and integer to the operating system*/
 {
-    puts("I am no longer a dummy.");
+    puts("Thrilled to meet you.");
     return 3;
 }
